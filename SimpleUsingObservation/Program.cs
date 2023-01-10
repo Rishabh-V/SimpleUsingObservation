6876b67f60511aac2be0b56039187aeb1c165dfd
Rishabh Verma <rishabhverma@google.com>
2023-01-11 00:35:09 +0530

    file scoped namespace.

--- SimpleUsingObservation/Program.cs
+++ SimpleUsingObservation/Program.cs
@@ -3,41 +3,39 @@ using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
 using System.Diagnostics;
 
-namespace SimpleUsingObservation
+namespace SimpleUsingObservation;
+internal class Program
 {
-    internal class Program
+    private const string AppName = "SimpleUsingObservation";
+    private const string Version = "1.0.0.0";
+    
+    private static readonly HttpClient s_httpClient = new();
+    private static readonly ActivitySource s_activitySource = new(AppName, Version);
+    
+    private static async Task Main(string[] args)
     {
-        private const string AppName = "SimpleUsingObservation";
-        private const string Version = "1.0.0.0";
-        
-        private static readonly HttpClient s_httpClient = new();
-        private static readonly ActivitySource s_activitySource = new(AppName, Version);
-        
-        private static async Task Main(string[] args)
-        {
-            string url = args.Length > 0 ? args[0] : "http://numbersapi.com/random/math?json";
+        string url = args.Length > 0 ? args[0] : "http://numbersapi.com/random/math?json";
 
-            var resourceBuilder = ResourceBuilder.CreateDefault().AddService(AppName, Version);
+        var resourceBuilder = ResourceBuilder.CreateDefault().AddService(AppName, Version);
 
-            // Initialize the OpenTelemetry TracerProvider.
-            _ = Sdk.CreateTracerProviderBuilder()
-                .SetResourceBuilder(resourceBuilder)
-                .AddSource(AppName)
-                .AddConsoleExporter()
-                .Build();
+        // Initialize the OpenTelemetry TracerProvider.
+        _ = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(resourceBuilder)
+            .AddSource(AppName)
+            .AddConsoleExporter()
+            .Build();
 
-            // This doesn't work with the simple using block but works with the scoped using block. Removing using also doesn't work. 
-            using var activity = s_activitySource.StartActivity();
-            activity?.SetTag("url", url);
-            var response = await GetDataAsync(url);
-            Console.ReadLine();
-        }
+        // This doesn't work with the simple using block but works with the scoped using block. Removing using also doesn't work. 
+        using var activity = s_activitySource.StartActivity();
+        activity?.SetTag("url", url);
+        var response = await GetDataAsync(url);
+        Console.ReadLine();
+    }
 
-        private static async Task<string> GetDataAsync(string url)
-        {  
-            var response = await s_httpClient.GetAsync(url);
-            response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
-        }
+    private static async Task<string> GetDataAsync(string url)
+    {  
+        var response = await s_httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadAsStringAsync();
     }
 }
